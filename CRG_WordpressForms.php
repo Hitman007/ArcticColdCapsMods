4764f457d532cf65a78002ce8488ef37fdf1fdcf
Hitman007 <support@customrayguns.com>
2015-09-30 02:56:18 -0400

    Update CRG_WordpressForms.php

--- CRG_WordpressForms.php
+++ CRG_WordpressForms.php
@@ -7,7 +7,7 @@ Version: 0.1
 Author: Jim Maguire
 Author URI: http://customrayguns.com/
 */
-
+	die('yu');
 	include_once('lib/CRG_FormController.class.php');
 	$CRG_FormController = new CRG_FormController;
 
