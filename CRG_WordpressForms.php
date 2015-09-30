43664bb41432a5120cdb144d24de768138df0b80
Hitman007 <support@customrayguns.com>
2015-09-30 09:55:17 -0400

    Update CRG_WordpressForms.php

--- CRG_WordpressForms.php
+++ CRG_WordpressForms.php
@@ -9,5 +9,6 @@ Author URI: http://customrayguns.com/
 */
 	include_once('CRG_FormController.class.php');
 	$CRG_FormController = new CRG_FormController;
+	add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 24;' ), 20 );
 
 ?>
