00f0ff83eb83ccbff43c835edc4c43ef5e57ff1d
Hitman007 <support@customrayguns.com>
2015-10-04 18:09:48 -0400

    Update CRG_WordpressForms.php

--- CRG_WordpressForms.php
+++ CRG_WordpressForms.php
@@ -10,4 +10,11 @@ Author URI: http://customrayguns.com/
 	include_once('CRG_FormController.class.php');
 	$CRG_FormController = new CRG_FormController;
 
-?>
+add_action( 'woocommerce_add_to_cart', 'custom_add_to_cart', 10, 2 );
+
+function custom_add_to_cart( $cart_item_key, $product_id ) {
+    // replace 123 with a valid product ID 
+    if( 485 == $product_id ) {
+        die('yo'); 
+    }
+}
