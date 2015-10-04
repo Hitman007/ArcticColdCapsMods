365410036cc6e2fdce1f8b40906d4cb923478e72
Hitman007 <support@customrayguns.com>
2015-10-04 03:38:16 -0400

    Update FormReciever.php

--- FormReciever.php
+++ FormReciever.php
@@ -3,16 +3,16 @@
 $listOfPostFieldNames = array();
 
 foreach (array_keys($_POST) as $fieldName){
+	if (!(isset($_POST["$fieldName"]))){break;}
 	$content = $content . $fieldName . " - ";
 	$content = $content . $_POST["$fieldName"];
 	$content = $content . "\r\n";
 }
 
 $title = $_POST['crg-info-form-last-name'] . ", " . $_POST['crg-info-form-first-name'];
-$my_post = array(
-				'post_title'    => $title,
-				'post_content'  => $content,
-				'post_type'   => 'InfoForm',
-			);
-			// Insert the post into the database
-			wp_insert_post( $my_post );
+$my_post = array(	'post_title'    => $title,
+			'post_content'  => $content,
+			'post_type'   => 'InfoForm',
+);
+// Insert the post into the database
+wp_insert_post( $my_post );
