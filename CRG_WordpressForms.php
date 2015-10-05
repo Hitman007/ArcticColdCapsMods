29c085d8c16e338f8a0f150b1096a9be8fef2580
Hitman007 <support@customrayguns.com>
2015-10-05 01:45:37 -0400

    Added shippping to cart when basic rental

--- CRG_WordpressForms.php
+++ CRG_WordpressForms.php
@@ -10,12 +10,11 @@ Author URI: http://customrayguns.com/
 	include_once('CRG_FormController.class.php');
 	$CRG_FormController = new CRG_FormController;
 
+//This adds shipping to the cart when the basic rental in added to the cart.
 add_action( 'woocommerce_add_to_cart', 'custom_add_to_cart', 10, 2 );
-
 function custom_add_to_cart( $cart_item_key, $product_id ) {
-    // replace 123 with a valid product ID 
     if( 485 == $product_id ) {
     	global $woocommerce;
-    	$woocommerce->cart->add_to_cart(670);
+    	$woocommerce->cart->add_to_cart(683);
     }
 }
