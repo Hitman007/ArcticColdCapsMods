1353e78e95efee915207bcbfead151951e024e46
Hitman007 <support@customrayguns.com>
2015-10-07 16:14:48 -0400

    Update receiveFormSubmissionFromHTMLrequest.php

--- receiveFormSubmissionFromHTMLrequest.php
+++ receiveFormSubmissionFromHTMLrequest.php
@@ -1,23 +1,22 @@
 <?php
 
-//This function is run when the user submits the POST variable ['crg-infoform-email']
+//This function is run when the user submits the POST variable ['crg-info-form']
 function receiveFormSubmissionFromHTMLrequest(){
+	$listOfPostFieldNames = array();
 
-$listOfPostFieldNames = array();
+	foreach (array_keys($_POST) as $fieldName){
+		$content = $content . $fieldName . " - ";
+		$content = $content . $_POST["$fieldName"];
+		$content = $content . "\r\n";
+	}	
 
-foreach (array_keys($_POST) as $fieldName){
-	$content = $content . $fieldName . " - ";
-	$content = $content . $_POST["$fieldName"];
-	$content = $content . "\r\n";
+	$title = $_POST['Last_Name'] . ", " . $_POST['First_Name'];
+	$my_post = array(	'post_title'    => $title,
+				'post_content'  => $content,
+				'post_type'   => 'InfoForm',
+	);
+	// Insert the post into the database
+	wp_insert_post( $my_post );
 }
 
-$title = $_POST['Last_Name'] . ", " . $_POST['First_Name'];
-$my_post = array(	'post_title'    => $title,
-			'post_content'  => $content,
-			'post_type'   => 'InfoForm',
-);
-// Insert the post into the database
-wp_insert_post( $my_post );
-
-
-}
+function putDataIntoCPT($array, $CPT_ID){}
