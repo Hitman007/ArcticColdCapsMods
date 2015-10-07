961e120d35f7b53f31834a091abfb8a59501686c
Hitman007 <support@customrayguns.com>
2015-10-06 20:10:04 -0400

    Update InfoFormForm.php

--- InfoFormForm.php
+++ InfoFormForm.php
@@ -278,31 +278,173 @@ h4
             </div>
 </div>
 <div class='form-group'>
-    <label class='col-sm-7'>Oncology Treatment Center:</label>
-    <div class='col-sm-5'>
-            <input  placeholder='Your oncology treatment center' name='Oncology Treatment Center' type='text' class='form-control'></div>
+ <label class='col-sm-5 control-label'>Oncologist Mailing Address:</label>
+    <div class='col-sm-7'>
+            <input  placeholder='Address' name='Oncologist_Address' type='text' class='form-control'>
+    </div>
+</div>
+<div class='form-group'>    
+<label class='col-sm-5 control-label'>City:</label>
+    <div class='col-sm-7'>
+            <div class='input-group'>
+            <span class='input-group-addon'><i class='glyphicon glyphicon-map-marker'></i></span>
+            <input  placeholder='City' name='Oncologist_City' type='text' class='form-control'>
+            </div>
+    </div>
 </div>
 <div class='form-group'>
-    <label class='col-sm-7'>What is the driving time in minutes to Oncologist office?</label>
-    <div class='col-sm-5'>
-            <div class='input-group'>
-            <span class='input-group-addon'><i class='glyphicon glyphicon-road'></i></span>
-            <input placeholder='Driving Time' name='Driving_Time' type='text' class='form-control'></div>
-            </div>
+    <label class='col-sm-5 control-label'>State:</label>
+    <div class='col-sm-7'>
+<select name = "Oncologist_State">
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+</select>
+    </div>
 </div>
-
+<div class='form-group'>
+    <label class='col-sm-5 control-label'>Zip:</label>
+    <div class='col-sm-7'>
+            <input  placeholder='Zip' name='Oncologist_Zip' type='text' class='form-control'>
+    </div>
+</div>
+<div class='form-group'>
+    <label class='col-sm-7'>Oncology Treatment Center:</label>
+    <div class='col-sm-5'>
+            <input  placeholder='Your oncology treatment center' name='Oncology_Treatment_Center' type='text' class='form-control'></div>
+</div>
+<div class='form-group'>
+ <label class='col-sm-5 control-label'>Mailing Address:</label>
+    <div class='col-sm-7'>
+            <input  placeholder='Address' name='Treatment_Address' type='text' class='form-control'>
+    </div>
+</div>
+<div class='form-group'>    
+<label class='col-sm-5 control-label'>City:</label>
+    <div class='col-sm-7'>
+            <div class='input-group'>
+            <span class='input-group-addon'><i class='glyphicon glyphicon-map-marker'></i></span>
+            <input  placeholder='City' name='Treatment_City' type='text' class='form-control'>
+            </div>
+    </div>
+</div>
+<div class='form-group'>
+    <label class='col-sm-5 control-label'>State:</label>
+    <div class='col-sm-7'>
+<select name = "Treatment_State">
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+</select>
+    </div>
+</div>
+<div class='form-group'>
+    <label class='col-sm-5 control-label'>Zip:</label>
+    <div class='col-sm-7'>
+            <input  placeholder='Zip' name='Treatment_Zip' type='text' class='form-control'>
+    </div>
+</div>
 <div class='form-group'>
     <label class='col-sm-7'>How many weeks apart are your chemo infusions scheduled for?</label>
     <div class='col-sm-5'>
             <input placeholder='Enter the weeks between infusions' name='Weeks_Between_Chemo' type='text' class='form-control'></div>
 </div>
-
-<div class='form-group'>
-    <label class='col-sm-7'>How Many Months is Your Chemo Regimen Scheduled For?</label>
-    <div class='col-sm-5'>
-            <input placeholder='Enter the length of your treatment' name='Length_Of_Treatment' type='text' class='form-control'></div>
-</div>
-
 <div class='form-group'>
     <div class='col-sm-12' align='right'>
             <input type='submit' class='btn btn-primary' >
