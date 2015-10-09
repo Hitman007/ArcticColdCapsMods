17364ac98d82b8e37e079761085767003adb5b8d
Hitman007 <support@customrayguns.com>
2015-10-08 21:57:01 -0400

    Update getFormSubmissionVariablesFromHTMLRequest.php

--- getFormSubmissionVariablesFromHTMLRequest.php
+++ getFormSubmissionVariablesFromHTMLRequest.php
@@ -5,19 +5,10 @@ namespace CustomRayGuns;
 //This function is run when the user submits the POST variable ['crg-info-form']
 function getFormSubmissionVariablesFromHTMLRequest(){
 	$listOfPostFieldNames = array();
-
+	
 	foreach (array_keys($_POST) as $fieldName){
-		$content = $content . $fieldName . " - ";
-		$content = $content . $_POST["$fieldName"];
-		$content = $content . "\r\n";
+		$listOfPostFieldNames[$fieldName] = $_POST[$fieldName];
 	}	
-
-	$title = $_POST['Last_Name'] . ", " . $_POST['First_Name'];
-	$my_post = array(	'post_title'    => $title,
-				'post_content'  => $content,
-				'post_type'   => 'InfoForm',
-	);
-	// Insert the post into the database
-	wp_insert_post( $my_post );
-	return "Happiness";
+	
+	return $listOfPostFieldNames;
 }
