39efec56032feaf0063d2603a8cf446eb670394c
Hitman007 <support@customrayguns.com>
2015-10-08 22:01:47 -0400

    Update getFormAuthor.php

--- getFormAuthor.php
+++ getFormAuthor.php
@@ -4,4 +4,11 @@ namespace CustomRayGuns;
 
 //this function will determine who's form is being modified or shown.
 
-function getFormAuthor(){}
+function getFormAuthor(){
+  
+  if (!(is_user_logged_in())){
+    return FALSE;
+  }
+  
+  return 5;
+}
