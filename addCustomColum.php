7cc39a382975f84f60068dfeda2504d06d793779
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-11 00:17:42 +0800

    Update addCustomColum.php

--- addCustomColum.php
+++ addCustomColum.php
@@ -8,7 +8,7 @@ add_filter( 'manage_users_columns', 'new_modify_user_table' );
 
 function new_modify_user_table_row( $val, $column_name, $user_id ) {
     $user = get_userdata( $user_id );
-    $link = "/info-sheet/?user=".$user;
+    $link = "/info-sheet/?user=".$user_id;
     switch ($column_name) {
         case 'userinfo' :
             return $link;
