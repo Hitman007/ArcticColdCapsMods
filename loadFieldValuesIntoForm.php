ca28e3b387a4c15a73a6aadd398e810bcd4f1a27
Hitman007 <support@customrayguns.com>
2015-10-10 12:06:04 -0400

    Update loadFieldValuesIntoForm.php

--- loadFieldValuesIntoForm.php
+++ loadFieldValuesIntoForm.php
@@ -5,7 +5,7 @@ function loadFieldValuesIntoForm(){
   //die('line 5 loadFieldValuesIntoForm.');
   $output = <<<direct_output
   <script>
-    alert('hi');
+    jQuery(document).ready(function(){alert('jQuery Hello');}
   </script>
 direct_output;
 }
