8d08ff0965bca2ca0fa8329937dd916b9dd92c06
Hitman007 <support@customrayguns.com>
2015-10-10 18:07:56 -0400

    Update ArcticColdCapsMods.php

--- ArcticColdCapsMods.php
+++ ArcticColdCapsMods.php
@@ -17,3 +17,13 @@ $FormController = new FormController;
 //Automatically adds standard shipping as a stand alone product to certain transactions.
 include_once('WooAutomaticallyAddShipping.php');
 WooAutomaticallyAddShipping();
+
+function auto_login_new_user( $user_id ) {
+        wp_set_current_user($user_id);
+        wp_set_auth_cookie($user_id);
+            // You can change home_url() to the specific URL,such as 
+        //wp_redirect( 'http://www.wpcoke.com' );
+        wp_redirect( home_url() );
+        exit;
+    }
+  add_action( 'user_register', 'auto_login_new_user' );
