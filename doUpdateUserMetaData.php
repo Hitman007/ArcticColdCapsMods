b8fa46fb803a78d166ea7df577882b8fb3799a96
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-11 03:02:06 +0800

    Update doUpdateUserMetaData.php

--- doUpdateUserMetaData.php
+++ doUpdateUserMetaData.php
@@ -1,6 +1,7 @@
 <?php
 
 function doUpdateUserMetaData($data){
+  var_dump($data);
   $user_id =  $data["UserID"];
   $meta = serialize($data);
   update_user_meta($user_id, 'CRG-FormInfo', $meta );
