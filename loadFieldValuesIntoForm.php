fc5ad2c899bdbb566f8498e3821f07ace841c96c
Hitman007 <support@customrayguns.com>
2015-10-10 12:23:34 -0400

    Update loadFieldValuesIntoForm.php

--- loadFieldValuesIntoForm.php
+++ loadFieldValuesIntoForm.php
@@ -3,11 +3,14 @@
 //the user WILL be logged in at this point. This script should directly echo a jQuery script
 function loadFieldValuesIntoForm(){
   //die('line 5 loadFieldValuesIntoForm.');
+  
   $output = <<<direct_output
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
-        alert('jQuery Hello line 10');
-
-  </script>
+jQuery('document')ready(function(){
+        alert('jQuery Hello line 11');
+});
+</script>
 direct_output;
 
   echo $output;
