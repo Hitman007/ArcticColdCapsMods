c71d9800c73703d0c689be4309eafcf873485802
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-11 03:00:20 +0800

    Update getFormSubmissionVariablesFromHTMLRequest.php

--- getFormSubmissionVariablesFromHTMLRequest.php
+++ getFormSubmissionVariablesFromHTMLRequest.php
@@ -9,15 +9,16 @@ function getFormSubmissionVariablesFromHTMLRequest(){
 	foreach (array_keys($_POST) as $fieldName){
 		$listOfPostFieldNames[$fieldName] = $_POST[$fieldName];
 	}
-	$user = get_current_user_id();
-	if ($user == 0){
+	$user_id = get_current_user_id();
+	if ($user_id == 0){
     		die( 'You are currently not logged in. getFormSubmissionVariablesFromHTMLRequest.php 14');
-		 }else{
-		 if(isset($_GET['user'])) {
-		    $user_id = $_GET['user'];
-		  } else {
-		    $user_id = get_current_user_id();
-		  }
+ 	} else {
+ 		$user_id = "";
+		if(isset($_GET['user'])) {
+			$user_id = $_GET['user'];
+		} else {
+			$user_id = get_current_user_id();
+		}
 		$listOfPostFieldNames['UserID'] = $user_id;
 	}
 	return $listOfPostFieldNames;
