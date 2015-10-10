762396e8ddca0710a25c15c075b7b8c9afa8c3f2
Hitman007 <support@customrayguns.com>
2015-10-09 20:38:04 -0400

    Update ArcticColdCapsMods.php

--- ArcticColdCapsMods.php
+++ ArcticColdCapsMods.php
@@ -14,11 +14,6 @@ namespace CustomRayGuns;
 include_once('FormController.class.php');
 $FormController = new FormController;
 
-//This adds shipping to the cart when the basic rental in added to the cart:
-add_action( 'woocommerce_add_to_cart', 'custom_add_to_cart', 10, 2 );
-function custom_add_to_cart( $cart_item_key, $product_id ) {
-    if( 485 == $product_id ) {
-    	global $woocommerce;
-    	$woocommerce->cart->add_to_cart(683);
-    }
-}
+//Automatically adds standard shipping as a stand alone product to certain transactions.
+include_once('WooAutomaticallyAddShipping.php');
+WooAutomaticallyAddShipping();
