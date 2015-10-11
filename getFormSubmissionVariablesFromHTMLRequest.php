d2d5d0343850eaa68478c43b88fe0ff24aad4355
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-12 01:13:58 +0800

    Update getFormSubmissionVariablesFromHTMLRequest.php

--- getFormSubmissionVariablesFromHTMLRequest.php
+++ getFormSubmissionVariablesFromHTMLRequest.php
@@ -13,10 +13,9 @@ function getFormSubmissionVariablesFromHTMLRequest(){
 	$user_id = get_current_user_id();
 	if ($user_id == 0){
     		//die( 'You are currently not logged in. getFormSubmissionVariablesFromHTMLRequest.php 14');
-    		$username = $listOfPostFieldNames['Email'];
     		$email = $listOfPostFieldNames['Email'];
     		$password = '1234';
-    		$user_id = wp_create_user( $username, $password, $email );
+    		$user_id = wp_create_user( $email, $password, $email );
     		
     		var_dump($user_id);
     		$listOfPostFieldNames['UserID'] = $user_id;
