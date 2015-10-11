bf6f3b8877eba660ba6b540d2750397380cbe8c4
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-12 02:08:57 +0800

    Update FormController.class.php

--- FormController.class.php
+++ FormController.class.php
@@ -69,13 +69,36 @@ class FormController{
 		$data = $this->formSubmissionVariables;
 		$email = $_POST['crg_login_email'];
 		$user = get_user_by( 'email', $email );
-		$content = serialize($data);
-		$my_post = array('post_title'    => $email,
+		//$content = serialize($data);
+		$title = $email;
+		
+		$form = get_page_by_title( $title, 'OBJECT', 'InfoForm' );
+		
+		if ($form) {
+			$my_post = array(
+				'ID'           => $form-ID,
+				'post_title'    => $title,
+				'post_content'  => $content,
+				'post_type'   => 'InfoForm',
+			);
+			
+			wp_update_post( $my_post );
+			
+			foreach($data as $k => $v) {
+				update_post_meta($form_id, $k, $v);
+			}
+		}else {
+			$my_post = array('post_title'    => $title,
 				'post_content'  => $content,
 				'post_author' => $user->ID,
 				'post_type'   => 'InfoForm',
-		);
-		// Insert the post into the database
-		$form_id = wp_insert_post( $my_post );
+			);
+			// Insert the post into the database
+			$form_id = wp_insert_post( $my_post );
+			
+			foreach($data as $k => $v) {
+				add_post_meta($form_id, $k, $v);
+			}
+		}
 	}
 }
