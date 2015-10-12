5fc836e61fd18636f3f4557d2194f21e43eec2a1
Hitman007 <support@customrayguns.com>
2015-10-12 12:05:49 -0400

    Update doUpdateRecord.php

--- doUpdateRecord.php
+++ doUpdateRecord.php
@@ -1,7 +1,38 @@
 <?php
 
 function doUpdateRecord($data){
-  $user_id =  $data["UserID"];
-  $meta = serialize($data);
-  update_user_meta($user_id, 'CRG-FormInfo', $meta );
+		$email = $_POST['crg_login_email'];
+		$user = get_user_by( 'email', $email );
+		$content = $_POST['notes'];
+		$title = $email;
+		
+		$form = get_page_by_title( $title, 'OBJECT', 'InfoForm' );
+		
+		if ($form) {
+			$my_post = array(
+				'ID'           => $form->ID,
+				'post_title'    => $title,
+				'post_author' => $user->ID,
+				'post_content'  => $content,
+				'post_type'   => 'InfoForm',
+			);
+			
+			wp_update_post( $my_post );
+			
+			foreach($data as $k => $v) {
+				update_post_meta($form->ID, $k, $v);
+			}
+		}else {
+			$my_post = array('post_title'    => $title,
+				'post_content'  => $content,
+				'post_author' => $user->ID,
+				'post_type'   => 'InfoForm',
+			);
+			// Insert the post into the database
+			$form_id = wp_insert_post( $my_post );
+			
+			foreach($data as $k => $v) {
+				add_post_meta($form_id, $k, $v);
+			}
+		}
 }
