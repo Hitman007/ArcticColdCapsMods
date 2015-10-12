f4be2e1156a6381abaaa2a9ce5f9906cd4a85169
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-13 04:12:33 +0800

    Update adminCPTviewMod.js

--- adminCPTviewMod.js
+++ adminCPTviewMod.js
@@ -1,8 +1,10 @@
 jQuery('document').ready(function() {
   jQuery("#the-list tr']").each(function() {
-    $val = jQuery('#the-list tr input').val();
+    var val = jQuery('#the-list tr input').val();
     
-    if($val === "UserID") {
+    alert(val);
+    
+    if(val === "UserID") {
       alert("FOUND!");
     }
     //jQuery('body.post-type-infoform span#view-post-btn a').html("View Info");
