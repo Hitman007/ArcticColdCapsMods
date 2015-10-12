d91110f01ec78caca2153f195ceb36afe30b54fb
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-13 01:46:04 +0800

    Update arcticColdCapsRoles.php

--- arcticColdCapsRoles.php
+++ arcticColdCapsRoles.php
@@ -4,4 +4,8 @@ namespace CustomRayGuns;
 
 function arcticColdCapsRoles(){
   //die('arcticColdCapsRoles.php');
+  
+  $role = get_role('shop_manager');
+  
+  add_role( 'nurse', 'Nurse', $role->capabilities );
 }
