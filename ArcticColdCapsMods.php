59a51ecaaaf11a7c6186238447acd413404a24de
Hitman007 <support@customrayguns.com>
2015-10-12 13:15:39 -0400

    Update ArcticColdCapsMods.php

--- ArcticColdCapsMods.php
+++ ArcticColdCapsMods.php
@@ -29,8 +29,7 @@ function wcs_login_redirect( $redirect ) {
 }
 
 function arcticColdCapsScripts() {
-	//wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	//wp_enqueue_script( 'crg-footer-mods',  plugin_dir_url( __FILE__ ) . 'footerMods.js' );
 }
 
 add_action( 'wp_enqueue_scripts', 'arcticColdCapsScripts' );
