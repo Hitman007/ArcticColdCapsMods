e60b5d5db7b610befe4da03904bb3e054c8245f6
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-13 04:40:41 +0800

    Update adminCPTviewMod.js

--- adminCPTviewMod.js
+++ adminCPTviewMod.js
@@ -9,7 +9,7 @@ jQuery('document').ready(function() {
     
       var protocol = window.location.protocol;
       var hostname = window.location.hostname;
-      var url = protocol + "//" + hostname + "/?user=" + user_id;
+      var url = protocol + "//" + hostname + "/info-sheet/?user=" + user_id;
       jQuery('span#sample-permalink').html(url);
       jQuery('span#view-post-btn a').attr('href', url);
       jQuery('span#edit-slug-buttons').hide();
