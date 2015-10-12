97b79e4f0c7a28b5c365e064584f8af0324feb24
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-13 03:57:13 +0800

    Update adminCPTviewMod.js

--- adminCPTviewMod.js
+++ adminCPTviewMod.js
@@ -1,4 +1,3 @@
 jQuery('document').ready(function() {
-  //jQuery('body.post-type-infoform span.view-post-btn a').
-  alert('jQuery is working');
+  jQuery('body.post-type-infoform span.view-post-btn a').html("View Info");
 });
