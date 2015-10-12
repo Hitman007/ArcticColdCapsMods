a7165d6e4a46cc9f1b43cf18150f95fdd3fdf6ed
Hitman007 <support@customrayguns.com>
2015-10-12 12:26:04 -0400

    Update addCustomColum.php

--- addCustomColum.php
+++ addCustomColum.php
@@ -10,7 +10,7 @@ add_filter( 'manage_users_columns', 'new_modify_user_table' );
 function new_modify_user_table_row( $val, $column_name, $user_id ) {
     $user = get_userdata( $user_id );
     $link = "/info-sheet/?user=".$user_id;
-    $user_link = "<a href='".get_site_url().$link."' target='_blank'>Info Sheet</a>";
+    $user_link = "<a href='".get_site_url().$link."' >Info Sheet</a>";
     switch ($column_name) {
         case 'userinfo' :
             return $user_link;
