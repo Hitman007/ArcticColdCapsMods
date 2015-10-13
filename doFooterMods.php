8c8f324b15774066d75ae425c557c3f70c8b2871
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-13 22:49:34 +0800

    Update doFooterMods.php

--- doFooterMods.php
+++ doFooterMods.php
@@ -4,6 +4,8 @@ $output = <<<scriptOutput
 <script>
     jQuery('document').ready(function(){
         //alert('$text');
+        var text = "This site is a <a href="http://customrayguns.com" target="_BLANK">Custom Ray Gun <img src="http://customrayguns.net/wp-content/plugins/crg_mods/images/ray_gun.png"></a>."
+        jQuery(div#sub-footer-core div.copyright).html(text).
     });
 </script>
 scriptOutput;
