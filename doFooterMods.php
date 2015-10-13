433d0a62e6eab633df767754a8ad4e6a5747cefa
Hitman007 <support@customrayguns.com>
2015-10-13 17:54:35 -0400

    Update doFooterMods.php

--- doFooterMods.php
+++ doFooterMods.php
@@ -3,7 +3,7 @@ $text = 'jQuery is working!';
 $output = <<<scriptOutput
 <script>
     jQuery('document').ready(function(){
-        var text = "This site is a <a href='http://customrayguns.com' target='_BLANK'>Custom Ray Gun <img src='http://customrayguns.net/wp-content/plugins/crg_mods/images/ray_gun.png'></a>";
+        var text = "This site is a <a href='http://customrayguns.com' target='_BLANK'>Custom Ray Gun <!-- <img src='http://customrayguns.net/wp-content/plugins/crg_mods/images/ray_gun.png'> --> </a>";
         jQuery('div#sub-footer-core div.copyright').html(text);
         jQuery('div#sub-footer-core div.copyright').css('float','right');
         jQuery('div#sub-footer-core div.copyright').css('text-align','right');
