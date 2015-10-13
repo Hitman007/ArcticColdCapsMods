37192e134b70a629959924ccec6c40b1eb6731b8
Hitman007 <support@customrayguns.com>
2015-10-13 16:59:34 -0400

    Update doFooterMods.php

--- doFooterMods.php
+++ doFooterMods.php
@@ -5,6 +5,8 @@ $output = <<<scriptOutput
     jQuery('document').ready(function(){
         var text = "This site is a <a href='http://customrayguns.com' target='_BLANK'>Custom Ray Gun <img src='http://customrayguns.net/wp-content/plugins/crg_mods/images/ray_gun.png'></a>";
         jQuery('div#sub-footer-core div.copyright').html(text);
+        jQuery('div#sub-footer-core div.copyright').css('float','right');
+        jQuery('div#sub-footer-core div.copyright').css('text-align','right');
     });
 </script>
 scriptOutput;
