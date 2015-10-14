cbf7d3ed7b8f235dda893d6b3970c394fbc294ec
Hitman007 <support@customrayguns.com>
2015-10-14 11:46:01 -0400

    Update ArcticColdCapsMods.php

--- ArcticColdCapsMods.php
+++ ArcticColdCapsMods.php
@@ -41,6 +41,10 @@ function loadFooterMods(){
     include_once('doFooterMods.php');
 }
 
+add_filter( 'auth_cookie_expiration', 'keep_me_logged_in_for_1_year' );
+function keep_me_logged_in_for_1_year( $expirein ) {
+    return 31556926; // 1 year in seconds
+}
 
 
 
