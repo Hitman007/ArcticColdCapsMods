51f72a60ec1dfe3814aabb6cdb4d275d4f7637ff
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 14:28:27 +0800

    Create addCRGDashboardMetaBox.php

---
+++ addCRGDashboardMetaBox.php
@@ -0,0 +1,18 @@
+<?php
+
+function addCRGDashboardMetaBox() {
+	add_meta_box(
+		'crg_dashboard_widget',		// ID
+		'Custom Ray Guns',		// Title
+		'crgDashboardWidgetFunction',	// Callback
+		'dashboard',			// Screen
+		'side',				// Context
+		'high'				// Priority
+	);
+}
+
+function crgDashboardWidgetFunction() {
+	include_once('doReturnAdminWidgetOutput.php');
+	$adminWidgetOutput = doReturnAdminWidgetOutput();
+	echo $adminWidgetOutput;
+}
