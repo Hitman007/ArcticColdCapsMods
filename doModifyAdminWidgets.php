265f1a7491fef6591a5581e314a564fcd2530194
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 11:38:43 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -6,3 +6,11 @@ function doModifyAdminWidgets(){
 	//die('hello wlrld');
         //die($adminWidgetOutput);
 }
+
+function myplugin_disable_screen_options( $show_screen ) {
+    if ( current_user_can( 'manage_options' ) ) {
+        return $show_screen;
+    }
+    return false;
+}
+add_filter( 'screen_options_show_screen', 'myplugin_disable_screen_options' );
