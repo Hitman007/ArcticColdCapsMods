06072cc25b206746a43aab42bb0bd50f9abd33aa
Hitman007 <support@customrayguns.com>
2015-10-16 12:15:45 -0400

    hi

--- doReturnAdminWidgetOutput.php
+++ doReturnAdminWidgetOutput.php
@@ -6,7 +6,7 @@ function doReturnAdminWidgetOutput(){
 How to add a new info-form <br/>
 How to edit an info-form <br/>
 How to change a person's email <br/>
-Get support
+Get support from Custom Ray Guns   
 WIDGETOUTPUT;
 
   return $widgetOutput;
