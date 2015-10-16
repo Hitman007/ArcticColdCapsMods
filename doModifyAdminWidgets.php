50bf2b7d5030cda6458dccc29e52e41b6de5e313
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 11:50:39 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -5,10 +5,4 @@ function doModifyAdminWidgets(){
 	$adminWidgetOutput = doReturnAdminWidgetOutput();
 	//die('hello wlrld');
         //die($adminWidgetOutput);
-        
-        wp_add_dashboard_widget(
-                 'example_dashboard_widget',         // Widget slug.
-                 'Example Dashboard Widget',         // Title.
-                 'example_dashboard_widget_function' // Display function.
-        );
 }
