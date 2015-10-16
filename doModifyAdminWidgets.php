0860796c4c8d68f76bac259bcb902540032aa2f6
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:46:09 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -2,18 +2,20 @@
 
 function doModifyAdminWidgets(){
 	
+	remove_action( 'welcome_panel', 'wp_welcome_panel' );
+	
 	//Normal Widgets (Left Side)
-	//remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' ); 	//Not sure
-        //remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );		//Plugins?
-        //remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );	//Not sure
-        //remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-        //remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' ); 	//Not sure
+        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );		//Removes Plugins Widget?
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );	//Not sure
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );	//Removes Comments Widget?
+        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );	//Removes At a Glance
         remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');		//Removes Activity
         
         //Side Widgets (Right Side)
         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );		//Remove WordPress News
         remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );	//Remove Quick Draft
-	//remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );	//Not sure
 	
 	// Remove help menu
 	add_filter( 'contextual_help', 'remove_contextual_help', 999, 3 );
@@ -23,7 +25,7 @@ function doModifyAdminWidgets(){
 	}
 	
 	// Remove Screen Options
-	//add_filter('screen_options_show_screen', 'remove_screen_options', 10, 2);
+	add_filter('screen_options_show_screen', 'remove_screen_options', 10, 2);
 	function remove_screen_options($display_boolean, $wp_screen_object){
 		$blacklist = array('index.php');
 		if (in_array($GLOBALS['pagenow'], $blacklist)) {
