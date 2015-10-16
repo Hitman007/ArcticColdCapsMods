9a2ce6583af87b54abfc3340db1825c7f27f78b4
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 11:59:36 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -1,6 +1,11 @@
 <?php
 
 function doModifyAdminWidgets(){
+	if (!current_user_can('manage_options') ) {
+		global $wp_meta_boxes;
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
+	
+	}
 	include_once('doReturnAdminWidgetOutput.php');
 	$adminWidgetOutput = doReturnAdminWidgetOutput();
 	//die('hello wlrld');
