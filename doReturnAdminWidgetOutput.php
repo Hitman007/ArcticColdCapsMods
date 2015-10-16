32a2573bb03365420276a3d2e1b9c9b9f16ccf2a
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 11:05:29 +0800

    Update doReturnAdminWidgetOutput.php

--- doReturnAdminWidgetOutput.php
+++ doReturnAdminWidgetOutput.php
@@ -1,13 +1,14 @@
 <?php
 
 function doReturnAdminWidgetOutput(){
+  /*
   $widgetOutput = <<<WIDGETOUTPUT
 How to add a new info-form
 How to edit an info-form
 How to change a person's email
 Get support
 WIDGETOUTPUT;
-
+*/
   return $widgetOutput;
   
 }
