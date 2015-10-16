80134a416343b70346f76cfc97e47c1ee2c7c9db
Hitman007 <support@customrayguns.com>
2015-10-15 23:28:30 -0400

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -3,6 +3,6 @@
 function doModifyAdminWidgets(){
 	include_once('doReturnAdminWidgetOutput.php');
 	$adminWidgetOutput = doReturnAdminWidgetOutput();
-	die('hello wlrld');
-        //die($adminWidgetOutput);
+	//die('hello wlrld');
+        die($adminWidgetOutput);
 }
