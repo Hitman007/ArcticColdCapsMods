bd91337e5a22cb11b3dfc728c09bd34b54a94d71
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 14:07:23 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -17,44 +17,5 @@ function doModifyAdminWidgets(){
         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );				//Remove WordPress News
         remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );			//Remove Quick Draft
 	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );			//Not sure
-	
-	// Remove help menu
-	add_filter( 'contextual_help', 'remove_contextual_help', 999, 3 );
-	function remove_contextual_help($old_help, $screen_id, $screen){
-		$screen->remove_help_tabs();
-		return $old_help;
-	}
-	
-	// Remove Screen Options
-	add_filter('screen_options_show_screen', 'remove_screen_options', 10, 2);
-	function remove_screen_options($display_boolean, $wp_screen_object){
-		$blacklist = array('index.php');
-		if (in_array($GLOBALS['pagenow'], $blacklist)) {
-			$wp_screen_object->render_screen_layout();
-			$wp_screen_object->render_per_page_options();
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-        add_action( 'wp_dashboard_setup', 'example_add_dashboard_widgets' );
-        function example_add_dashboard_widgets() {
-		add_meta_box(
-	                 'crg_dashboard_widget',		// ID
-	                 'Custom Ray Guns',			// Title
-	                 'crgDashboardWidgetFunction',		// Callback
-	                 'dashboard',				// Screen
-	                 'side',				// Context
-	                 'high'					// Priority
-	                 
-	        );
-	}
-
-	function crgDashboardWidgetFunction() {
-		include_once('doReturnAdminWidgetOutput.php');
-		$adminWidgetOutput = doReturnAdminWidgetOutput();
-		echo $adminWidgetOutput;
-	}
 
 }
