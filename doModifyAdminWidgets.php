4b771514bb8505189041669a57c6d8524807b3e5
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:43:16 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -8,7 +8,7 @@ function doModifyAdminWidgets(){
         //remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );	//Not sure
         //remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
         //remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-        //remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');		//Removes Activity
         
         //Side Widgets (Right Side)
         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );		//Remove WordPress News
