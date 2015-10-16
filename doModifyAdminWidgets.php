2b943758e728bcee1d3eb16450256573f7841ef9
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:19:50 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -12,10 +12,18 @@ function doModifyAdminWidgets(){
 	}
 	
 	// Remove Screen Options
-	add_filter( 'screen_options_show_screen', 'myplugin_disable_screen_options' );
-	function myplugin_disable_screen_options( $show_screen ) {
-		return $show_screen;
+	add_filter('screen_options_show_screen', 'remove_screen_options', 10, 2);
+	function remove_screen_options($display_boolean, $wp_screen_object){
+		$blacklist = array('index.php');
+		if (in_array($GLOBALS['pagenow'], $blacklist)) {
+			$wp_screen_object->render_screen_layout();
+			$wp_screen_object->render_per_page_options();
+			return false;
+		} else {
+			return true;
+		}
 	}
+	
 	
 	include_once('doReturnAdminWidgetOutput.php');
 	$adminWidgetOutput = doReturnAdminWidgetOutput();
