15f7da5b5e0cddc8bcb5a82bf27cd241fa5e59af
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:37:16 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -5,8 +5,8 @@ function doModifyAdminWidgets(){
 	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' ); 	//Not sure
         remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );		//Plugins?
         remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );		//Remove WordPress News
-        //remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );	//Not sure
+        //remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
         remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
         remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
         remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
