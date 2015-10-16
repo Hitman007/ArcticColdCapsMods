e4159bba05f88fb08ba1702e5d7ab303b4ee64a2
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:15:38 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -7,8 +7,13 @@ function doModifyAdminWidgets(){
 	// Remove help menu
 	add_filter( 'contextual_help', 'remove_contextual_help', 999, 3 );
 	function remove_contextual_help($old_help, $screen_id, $screen){
-	             $screen->remove_help_tabs();
-	             return $old_help;
+		$screen->remove_help_tabs();
+		return $old_help;
+	}
+	// Remove Screen Options
+	add_filter( 'screen_options_show_screen', 'myplugin_disable_screen_options' );
+	function myplugin_disable_screen_options( $show_screen ) {
+		return $show_screen;
 	}
 	
 	include_once('doReturnAdminWidgetOutput.php');
