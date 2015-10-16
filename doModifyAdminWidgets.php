b16ba13d62c6c0273a6c516a7f7d4832811a01b6
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-16 12:09:25 +0800

    Update doModifyAdminWidgets.php

--- doModifyAdminWidgets.php
+++ doModifyAdminWidgets.php
@@ -1,6 +1,7 @@
 <?php
 
 function doModifyAdminWidgets(){
+	remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
 	include_once('doReturnAdminWidgetOutput.php');
 	$adminWidgetOutput = doReturnAdminWidgetOutput();
 	//die('hello wlrld');
