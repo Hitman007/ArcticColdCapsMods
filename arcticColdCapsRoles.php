e9ee2b03e55d5cd09214a9d21d3c0f08cda54401
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-19 04:07:41 +0800

    Update arcticColdCapsRoles.php

--- arcticColdCapsRoles.php
+++ arcticColdCapsRoles.php
@@ -8,10 +8,10 @@ function arcticColdCapsRoles(){
 	global $wp_roles;
 	if ( ! isset( $wp_roles ) )
 		$wp_roles = new WP_Roles();
-	$wp_roles->roles['subscriber']['name'] = 'Customer';
-	$wp_roles->role_names['subscriber'] = 'Customer';
-	$wp_roles->roles['shop_manager']['name'] = 'Nurse';
-	$wp_roles->role_names['shop_manager'] = 'Nurse';
+	//$wp_roles->roles['subscriber']['name'] = 'Customer';
+	//$wp_roles->role_names['subscriber'] = 'Customer';
+	//$wp_roles->roles['shop_manager']['name'] = 'Nurse';
+	//$wp_roles->role_names['shop_manager'] = 'Nurse';
   
   //$role = get_role('shop_manager');
   //add_role( 'nurse', 'Nurse', $role->capabilities );
