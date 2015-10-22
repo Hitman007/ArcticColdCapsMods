0ebe00f4445909628cd7130d6459a74ea41620ee
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-23 00:02:53 +0800

    Update addCustomColumn.php

--- addCustomColumn.php
+++ addCustomColumn.php
@@ -42,10 +42,6 @@ function add_bill_info( $val, $column_name, $user_id ) {
     
     $bill_info = <<<OUTPUT
     <input type='checkbox' name='bill_info' id='bill_info' class='bill-info-$user_id />
-    <div class='bill-info-$user_id' style='display: none;'>
-    <form id='bill_info_$user_id>
-    <textarea></textarea>
-    </form></div>
 OUTPUT;
     /*
     if($form) {
