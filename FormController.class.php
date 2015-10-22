ae288c31a476f50f7075d112f0958a07ad5cacfc
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-23 00:29:50 +0800

    Update FormController.class.php

--- FormController.class.php
+++ FormController.class.php
@@ -13,7 +13,7 @@ class FormController{
 		//This loads the script that adds a link to the admin area
 		include_once('addCustomColumn.php');
 		include_once('getUserInfoScript.php');
-		getUserInfoScript();
+		echo getUserInfoScript();
 
 		//create the CPT 'info-form'
 		include_once('InfoFormCPT.class.php');
