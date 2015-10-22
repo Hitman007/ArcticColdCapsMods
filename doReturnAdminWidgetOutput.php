977565f65ed13710b75ca7da8841a2621f46c0fe
Hitman007 <support@customrayguns.com>
2015-10-22 12:43:32 -0400

    Update doReturnAdminWidgetOutput.php

--- doReturnAdminWidgetOutput.php
+++ doReturnAdminWidgetOutput.php
@@ -6,19 +6,19 @@ function doReturnAdminWidgetOutput(){
   <div id = "crg-help-widget-image-div">
   </div>
   <div class = "crg-help-widget-item">
-	How to add a new info-form
+	<a href = "https://www.youtube.com/watch?v=xgwuu17z4iE" targer = "_blank">How to add a new info-form</a>
   </div>
   <div class = "crg-help-widget-item">
 	<a href = "https://www.youtube.com/watch?v=rxpaLeKrL2E&feature=youtu.be" target = "_blank">How to edit an info-form</a>
   </div>
   <div class = "crg-help-widget-item">
-  	How to change a person's email
+  	<a href = "https://www.youtube.com/watch?v=HXPoQ3-cSGI&feature=youtu.be">How to change a person's email</a>
   </div>
   <div class = "crg-help-widget-item">
   	How to turn recurring billing on and off
   </div>
   <div class = "crg-help-widget-item">
-  	Ask Custom Ray Guns a quetion
+  	<a href = "http://customrayguns.com/contact/" target = "_blank">Ask Custom Ray Guns a question</a>
   </div>
 <script>
 jQuery('document').ready(function(){
