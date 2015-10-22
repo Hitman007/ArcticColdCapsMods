08d8a81a20a7770dcc8fffe0df4d66be79675b67
Hitman007 <support@customrayguns.com>
2015-10-22 14:08:46 -0400

    Update getUserInfoScript.php

--- getUserInfoScript.php
+++ getUserInfoScript.php
@@ -2,12 +2,19 @@
 
 function getUserInfoScript() {
     $output = <<<OUTPUT
+        <link rel='stylesheet' href='//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css'>
         <script>
         jQuery('document').ready(function() {
             jQuery('input.bill_info').click(function() {
                 var div = jQuery(this).attr('id');
             	jQuery('div#' + div).toggle();
             	jQuery('div#' + div).html("<form>Next Billing Date: <input type = 'text' name = 'next-billing-date' > Treatments Remaining: <input type = 'text' name = 'treatments-remaining' size = '2' /> <input type = 'submit' value = 'UPDATE' name = 'recurring-billinging-submit' /></form>");
+            	jQuery('div#' + div).datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    maxDate: "+5y",
+                    minDate: new Date()
+                });
             });
         });
         </script>
@@ -15,3 +22,8 @@ OUTPUT;
     
     echo $output;
 }
+
+
+
+
+
