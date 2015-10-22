f3c6822fc24ea2c8ca33349b80c3c9429c9c06dd
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-23 01:04:04 +0800

    Update and rename getUserInfoScript.js to getUserInfoScript.php

--- getUserInfoScript.js
+++
@@ -1,8 +0,0 @@
-jQuery('document').ready(function() {
-    alert('loaded');
-    //jQuery('#bill_info').click(function() {
-    //	$(this).next("#bill_info_form").toggle();
-    //});
-});
-
-
---
+++ getUserInfoScript.php
@@ -0,0 +1,14 @@
+<?php
+
+$output = <<<OUTPUT
+    <script>
+    jQuery('document').ready(function() {
+        alert('loaded');
+        //jQuery('#bill_info').click(function() {
+        //	$(this).next("#bill_info_form").toggle();
+        //});
+    });
+    </script>
+OUTPUT;
+
+echo $output;
