84a96b104a1a299a88be6ae6e9fb13aa56af0635
Hitman007 <support@customrayguns.com>
2015-10-22 14:15:29 -0400

    Update getUserInfoScript.php

--- getUserInfoScript.php
+++ getUserInfoScript.php
@@ -2,14 +2,14 @@
 
 function getUserInfoScript() {
     $output = <<<OUTPUT
-     <script src='//code.jquery.com/ui/1.11.4/jquery-ui.js'></script>
+        <link rel='stylesheet' href='//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css'>
         <script>
         jQuery('document').ready(function() {
             jQuery('input.bill_info').click(function() {
                 var div = jQuery(this).attr('id');
-            	jQuery('div#' + div).slideDown();
-            	jQuery('div#' + div).html("<form>Next Billing Date: <input type = 'text' name = 'next-billing-date' > Treatments Remaining: <input type = 'text' name = 'treatments-remaining' size = '2' /> <input type = 'submit' value = 'UPDATE' name = 'recurring-billinging-submit' /></form>");
-            	jQuery('div#' + div).datepicker({
+            	jQuery('div#' + div).slideDown('slow');
+            	jQuery('div#' + div).html("<form>Next Billing Date: <input type = 'text' name = 'next-billing-date' class = 'dp' > Treatments Remaining: <input type = 'text' name = 'treatments-remaining' size = '2' /> <input type = 'submit' value = 'UPDATE' name = 'recurring-billinging-submit' /></form>");
+            	jQuery('.dp').datepicker({
                     changeMonth: true,
                     changeYear: true,
                     maxDate: "+5y",
