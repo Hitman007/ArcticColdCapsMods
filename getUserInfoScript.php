ba68593ca20ae1aa63dab251efb710277fa108d9
CRG-Freelance1 <freelance1@customrayguns.net>
2015-10-23 00:31:09 +0800

    Update getUserInfoScript.php

--- getUserInfoScript.php
+++ getUserInfoScript.php
@@ -4,9 +4,13 @@ function getUserInfoScript() {
     $output = <<<SCRIPTOUTPUT
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script>
-        jQuery('#bill_info').click(function() {
-        	$(this).next("#bill_info_form").toggle();
+        jQuery(document).ready(function() {
+            alert('loaded');
+            //jQuery('#bill_info').click(function() {
+            //	$(this).next("#bill_info_form").toggle();
+            //});
         });
+        
     </script>
 SCRIPTOUTPUT;
     
