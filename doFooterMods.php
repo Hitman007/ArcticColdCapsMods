72c76d9b0426574cd7145200ae635a5cc7c13f68
Hitman007 <support@customrayguns.com>
2015-11-06 14:21:38 -0500

    Update doFooterMods.php

--- doFooterMods.php
+++ doFooterMods.php
@@ -3,7 +3,7 @@
 $output = <<<scriptOutput
 <script>
     jQuery('document').ready(function(){
-        var text = "This site is a <a href='http://customrayguns.com' target='_BLANK'>Custom Ray Gun <img src='https://arcticcoldcaps.com/wp-content/plugins/CRG-ArcticColdCapsMods/ray_gun.gif'> </a>";
+        var text = "Copyright (C), 2015 Arctic Cold Caps, LLC.";
         jQuery('div#sub-footer-core div.copyright').html(text);
         jQuery('div#sub-footer-core div.copyright').css('float','right');
         jQuery('div#sub-footer-core div.copyright').css('text-align','right');
