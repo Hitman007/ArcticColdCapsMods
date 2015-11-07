6fed3f74ba0b67f57fafdd22257c38234507f754
Hitman007 <support@customrayguns.com>
2015-11-07 14:14:22 -0500

    Update returnConversionFunnelStartShortcode.php

--- returnConversionFunnelStartShortcode.php
+++ returnConversionFunnelStartShortcode.php
@@ -1,11 +1,12 @@
 <?php
 
 function returnConversionFunnelStartShortcode(){
-  if ( is_user_logged_in() ) {echo ('
-    <script type="text/javascript">
-window.location = "http://www.google.com/"
-</script>
-');}
+  if ( is_user_logged_in() ) {
+    echo ('
+      <script type="text/javascript">
+        window.location = "http://www.google.com/"
+      </script>
+  ');}
   
 $shortCodeOutput = <<<SHORT_CODE_OUTPUT
 <form action = "/my-account">
