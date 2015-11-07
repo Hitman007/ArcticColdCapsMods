bcd8c6d5e5f0d8b222ff43348fbabd494441846f
Hitman007 <support@customrayguns.com>
2015-11-07 13:52:20 -0500

    Update returnConversionFunnelStartShortcode.php

--- returnConversionFunnelStartShortcode.php
+++ returnConversionFunnelStartShortcode.php
@@ -1,6 +1,14 @@
 <?php
 
 function returnConversionFunnelStartShortcode(){
+  
+$output = <<<SHORT_CODE_OUTPUT
+<form action = "/my-account">
+<input type = "submit" value = "Returning User" />
+</form>
+New User
+
+SHORT_CODE_OUTPUT;
 
   return ('hithere');
   
