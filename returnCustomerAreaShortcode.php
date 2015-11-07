233a0acd241b40560a817b59ea50ea14596f812d
Hitman007 <support@customrayguns.com>
2015-11-06 22:05:20 -0500

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -2,10 +2,14 @@
 
 function returnCustomerAreaShortcode(){
   
-$shortcodeOutput = <<<shortcodeOutput
+  $shortcodeOutput = <<<shortcodeOutput
 <h2>Customer Area</h2>
-Information Form
-HIPPA Form
+<br />
+Please fill out the following forms:<br />
+<a href = "">Information Form</a>
+<a href = "">HIPPA Form</a>
 shortcodeOutput;
+  
   return $shortcodeOutput;
+
 }
