582701ac5b11253067f2ceaaddcdb1887e1f1f13
Hitman007 <support@customrayguns.com>
2015-11-07 13:38:48 -0500

    Update ArcticColdCapsMods.php

--- ArcticColdCapsMods.php
+++ ArcticColdCapsMods.php
@@ -91,15 +91,16 @@ if (isset($_POST['crg_quick_phone'])){
 	add_action('init', 'CustomRayGuns\quickForm');
 }
 
+//This is the Welcome page
+add_shortcode('conversion-funnel-start', 'CustomRayGuns\loadConversionFunnelStartShortcode');
+function loadConversionFunnelStartShortcode(){
+	require 'returnConversionFunnelStartShortcode.php';
+	return returnConversionFunnelStartShortcode();
+}
+
 //This is the Woo Commerce "my-account" page
 add_shortcode('customer-area', 'CustomRayGuns\loadReturnCustomerAreaShortcode');
 function loadReturnCustomerAreaShortcode(){
 	require 'returnCustomerAreaShortcode.php';
 	return returnCustomerAreaShortcode();
-}
-
-add_shortcode('conversion-funnel-start', 'CustomRayGuns\loadConversionFunnelStartShortcode');
-function loadConversionFunnelStartShortcode(){
-	require 'returnConversionFunnelStartShortcode.php';
-	return returnConversionFunnelStartShortcode();
 }
