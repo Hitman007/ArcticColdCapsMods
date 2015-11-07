b6485af57ef5cda39ae7c958a541061b929b4c2f
Hitman007 <support@customrayguns.com>
2015-11-07 14:11:47 -0500

    Update returnConversionFunnelStartShortcode.php

--- returnConversionFunnelStartShortcode.php
+++ returnConversionFunnelStartShortcode.php
@@ -1,8 +1,11 @@
 <?php
 
 function returnConversionFunnelStartShortcode(){
-  if ( is_user_logged_in() ) {wp_redirect("http://google.com" );
-exit;}
+  if ( is_user_logged_in() ) {echo ('
+    <script type="text/javascript">
+window.location = "http://www.google.com/"
+</script>
+');}
   
 $shortCodeOutput = <<<SHORT_CODE_OUTPUT
 <form action = "/my-account">
