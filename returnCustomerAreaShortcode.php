f96a3516c43405e2503b4c5eceb4934cc96eca47
Hitman007 <support@customrayguns.com>
2015-11-06 22:11:16 -0500

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -8,6 +8,9 @@ function returnCustomerAreaShortcode(){
 Please fill out the following forms:<br />
 <a href = "/info-sheet/">Information Form</a><br />
 <a href = "">HIPPA Form</a>
+<a href = "/tac">Terms and Conditions</a><br />
+<a href = "/hipaa">Artic Cold Caps HIPAA Authorization Form</a><br />
+<a href = ""></a><br />
 shortcodeOutput;
   
   return $shortcodeOutput;
