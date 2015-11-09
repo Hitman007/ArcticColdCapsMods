956f9e5c506b31c5f5e357eb66d8df1e45c6ec01
CRG-Freelance1 <freelance1@customrayguns.net>
2015-11-10 04:51:34 +0800

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -4,15 +4,11 @@ function returnCustomerAreaShortcode(){
     if ( is_user_logged_in() ) {
         $user_id = get_current_user_id();
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
-        if( isset($_POST['user_accept_terms_true_or_false']) ){
-            $terms_post = $_POST['user_accept_terms_true_or_false'];
-            if(!empty($terms)) {
-                update_user_meta($user_id, "user_accept_terms_true_or_false", $terms_post);
-            } else {
-                add_user_meta( $user_id, "user_accept_terms_true_or_false", $terms_post);
-            }
-        }else{
-            update_user_meta($user_id, "user_accept_terms_true_or_false", false);
+        $terms_post = $_POST['user_accept_terms_true_or_false'];
+        if(!empty($terms)) {
+            update_user_meta($user_id, "user_accept_terms_true_or_false", $terms_post);
+        } else {
+            add_user_meta( $user_id, "user_accept_terms_true_or_false", $terms_post);
         }
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
         echo "<input type='hidden' id='tac_meta' name='user_accept_terms_true_or_false_meta' value='".$terms[0]."'>";
