a1ca096fb46f7b519c85216407143a7ff15c619a
CRG-Freelance1 <freelance1@customrayguns.net>
2015-11-10 05:21:26 +0800

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -13,11 +13,8 @@ function returnCustomerAreaShortcode(){
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
             $terms_post = $_POST['user_accept_terms_true_or_false'];
             update_user_meta($user_id, "user_accept_terms_true_or_false", $terms_post);
-        } else {
-            $terms_post = $_POST['user_accept_terms_true_or_false'];
-            update_user_meta($user_id, "user_accept_terms_true_or_false", $terms_post);
         }
-
+        
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
         echo "2".$terms[0]."2";
         echo "<input type='hidden' id='tac_meta' name='user_accept_terms_true_or_false_meta' value='".$terms[0]."'>";
