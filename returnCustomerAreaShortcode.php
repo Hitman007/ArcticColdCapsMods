a39ae8e888d82132107d1fa8e514bc3a11b2f215
CRG-Freelance1 <freelance1@customrayguns.net>
2015-11-10 04:21:01 +0800

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -5,7 +5,7 @@ function returnCustomerAreaShortcode(){
         $user_id = get_current_user_id();
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
         if(!empty($terms)) {
-            die("Get meta value");
+            echo "<input type='hidden' name='user_accept_terms_true_or_false' value='".$terms."'>";
         } else {
             add_user_meta( $user_id, 'user_accept_terms_true_or_false', false);
         }
