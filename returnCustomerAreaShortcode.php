d71135aee321efba1f619c1a1a4455b65ee21926
CRG-Freelance1 <freelance1@customrayguns.net>
2015-11-10 05:13:41 +0800

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -4,7 +4,7 @@ function returnCustomerAreaShortcode(){
     if ( is_user_logged_in() ) {
         $user_id = get_current_user_id();
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
-        echo $terms[0];
+        echo "1".$terms[0]."1";
         if(empty($terms)) {
             $terms_post = $_POST['user_accept_terms_true_or_false'];
             add_user_meta( $user_id, "user_accept_terms_true_or_false", $terms_post);
@@ -19,7 +19,7 @@ function returnCustomerAreaShortcode(){
         }
 
         $terms = get_user_meta($user_id, "user_accept_terms_true_or_false");
-        echo $terms[0];
+        echo "2".$terms[0]."2";
         echo "<input type='hidden' id='tac_meta' name='user_accept_terms_true_or_false_meta' value='".$terms[0]."'>";
         $shortcodeOutput = <<<shortcodeOutput
 <h2>Please review the following:</h2>
