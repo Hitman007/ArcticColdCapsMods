518001fda99ccc14de63d47197969ebe9337ed69
CRG-Freelance1 <freelance1@customrayguns.net>
2015-11-10 04:48:37 +0800

    Update returnCustomerAreaShortcode.php

--- returnCustomerAreaShortcode.php
+++ returnCustomerAreaShortcode.php
@@ -36,7 +36,7 @@ jQuery(document).ready(function(){
         var val = jQuery("#tac_check").val();
     });
     var meta = jQuery("input#tac_meta").val();
-    if (meta == 1) {
+    if (meta == 'on') {
         jQuery("#tac_check").prop('checked', true);
     } else {
         jQuery("#tac_check").prop('checked', false);
