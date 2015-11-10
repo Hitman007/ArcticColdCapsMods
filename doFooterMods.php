0e57eee90d3e278144a8bfeded532eee2ffae61e
Hitman007 <support@customrayguns.com>
2015-11-10 02:54:22 -0500

    Update doFooterMods.php

--- doFooterMods.php
+++ doFooterMods.php
@@ -12,7 +12,6 @@ $output = <<<scriptOutput
 scriptOutput;
 $x = home_url();
 if ($x == "https://arcticcoldcaps.com"){
-    die("yup");
     $output = $output . "<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
