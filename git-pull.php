13337ca9be0ebb6e9ac8970d88c9cb5467b111de
Hitman007 <support@customrayguns.com>
2015-11-13 16:45:40 -0500

    Update git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('ls');
+
+$output = shell_exec('git pull origin master');
 echo "<pre>$output</pre>";
-//$output = shell_exec('git pull origin master');
-//echo "<pre>$output</pre>";
+
 ?>
