bdfd75f1e3693fae6f9e30f8768ffa4165d21aba
Hitman007 <support@customrayguns.com>
2016-01-30 08:56:01 -0800

    Update doReturnAdminWidgetOutput.php

--- doReturnAdminWidgetOutput.php
+++ doReturnAdminWidgetOutput.php
@@ -5,6 +5,12 @@ function doReturnAdminWidgetOutput(){
   $widgetOutput = <<<WIDGETOUTPUT
   <div id = "crg-help-widget-image-div">
   </div>
+   <div class = "crg-help-widget-item">
+  	<a href = "https://youtu.be/cS5j_UcIVaA" target = "_blank">How to process a refund</a>
+  </div>
+     <div class = "crg-help-widget-item">
+  	<a href = "https://youtu.be/TWYSobESEVg" target = "_blank">How to give a discount coupoun</a>
+  </div>
   <div class = "crg-help-widget-item">
   	<a href = "https://www.youtube.com/watch?v=HXPoQ3-cSGI&feature=youtu.be">How to change a person's email</a>
   </div>
